fda79dbe798387f8bdb37c27e7e23a4203f74c47
jclizar <jessica_lizar@yahoo.com>
2024-10-13 19:42:13 -0300

    teste into

---
+++ Introdução a teste no Python/test_exercise.py
@@ -0,0 +1,33 @@
+def str_to_bool(value):
+    try:
+        value = value.lower()
+    except AttributeError:
+        raise AttributeError(f"{value} must be of type string")
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
+    
+import unittest
+
+class TestStrToBool(unittest.TestCase):
+
+    def test_y_is_true(self):
+        result = str_to_bool('y')
+        self.assertTrue(result)
+
+    def test_yes_is_true(self):
+        result = str_to_bool('Yes')
+        self.assertTrue(result)
+
+    def test_invalid_input(self):
+        with self.assertRaises(AttributeError): # estou esprando um erro do tipo AttributeError
+            # se o teste PASSAR é pq teve um erro do tipo AttributeError
+            str_to_bool(1)
+
+        
+if __name__ == '__main__':
+    unittest.main()
